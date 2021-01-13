9445095d2c9be4891358202d86d6a1e08c42aec7
JenniferPeart <jenniferpeart@hotmail.com>
2021-01-13 18:30:33 +0000

    Display users list - Angular mock data

---
+++ ums-frontend/src/app/mock-users.ts
@@ -0,0 +1,10 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+    { id: 1, name: 'Jen', email: 'jen@gamil.com' },
+    { id: 2, name: 'Ben', email: 'ben@gamil.com' },
+    { id: 3, name: 'Hen', email: 'hen@gamil.com' },
+    { id: 4, name: 'Fen', email: 'fen@gamil.com' },
+    { id: 5, name: 'Den', email: 'den@gamil.com' },
+    { id: 6, name: 'Pen', email: 'pen@gamil.com' },
+];

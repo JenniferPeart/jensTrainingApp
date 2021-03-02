1b6ea8f39e4ad9aee6d7d744f6b39b7d99a80edd
Jennifer Peart <jennifer.peart@admiralgroup.co.uk>
2021-03-02 17:08:16 +0000

    WIP: Add authentication

--- ums-frontend/src/app/app.component.ts
+++ ums-frontend/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { UserService } from './services/user.service';
+import { SecurityService } from './services/security.service';
+import { Credentials } from './interfaces/credentials';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'User Management System';
+
+  credentials: Credentials = {
+    username: 'jen',
+    password: 'password',
+    isAuthenticated: false,
+    isAdmin: true
+  };
+
+  constructor(
+    private userService: UserService, private securityService: SecurityService, private router: Router, private http: HttpClient) {}
+
 }
--- ums-frontend/src/app/app.module.ts
+++ ums-frontend/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
-import { HttpClientModule } from '@angular/common/http';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   // the components, directives and pipes that belong to this NgModule
@@ -14,14 +16,16 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     AppComponent,
     UsersComponent,
     EditUserComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LogInComponent
   ],
   // other modules whose exported classes are needed ny component templates
   // declared in this NgModule
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   // creators of services that this NgModule contributes to the global
   // collection of services; they become accessible in all parts of the app
